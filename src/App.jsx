3bc2f1e6f50a293009eebd42a236543b5fd2afd8
Sara <sarailic431@gmail.com>
2025-07-14 21:23:42 +0200

    fetching data with tan stack query

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,78 @@
 import { useEffect, useState } from "react";
 import "./App.css";
+import {
+  QueryClient,
+  QueryClientProvider,
+  useQuery,
+} from "@tanstack/react-query";
 
-function App() {
-  const [joke, setJoke] = useState(null);
-  useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const response = await fetch("https://icanhazdadjoke.com/", {
-          headers: {
-            Accept: "application/json",
-          },
-        });
+const queryClient = new QueryClient();
 
-        const data = await response.json();
-        console.log(data);
-        setJoke(data.joke);
-      } catch (error) {
-        console.log("Fetch error:", error);
-      }
-    };
+// function App() {
+//   const [joke, setJoke] = useState(null);
+//   useEffect(() => {
+//     const fetchPost = async () => {
+//       try {
+//         const response = await fetch("https://icanhazdadjoke.com/", {
+//           headers: {
+//             Accept: "application/json",
+//           },
+//         });
 
-    fetchPost();
-  }, []);
+//         const data = await response.json();
+//         console.log(data);
+//         setJoke(data.joke);
+//       } catch (error) {
+//         console.log("Fetch error:", error);
+//       }
+//     };
 
+//     fetchPost();
+//   }, []);
+
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <>
+//         <h1>DAD JOKES</h1>
+//         {joke && <h3>{joke}</h3>}
+//       </>
+//     </QueryClientProvider>
+//   );
+// }
+
+// export default App;
+
+export default function App() {
   return (
-    <>
-      <h1>DAD JOKES</h1>
-      {joke && <h3>{joke}</h3>}
-    </>
+    <QueryClientProvider client={queryClient}>
+      <Example />
+    </QueryClientProvider>
   );
 }
 
-export default App;
+function Example() {
+  const [joke, setJoke] = useState(null);
+  const { isPending, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      }).then((res) => res.json()),
+  });
+  console.log(data);
+  // setJoke(data.joke);
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <>
+        <h1>DAD JOKES</h1>
+        {joke && <h3>{joke}</h3>}
+      </>
+    </QueryClientProvider>
+  );
+}
