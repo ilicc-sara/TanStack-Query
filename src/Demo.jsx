568f585080d4ca2c38ff8c6b24743a0a2cfe7d08
Sara <sarailic431@gmail.com>
2025-07-14 22:19:12 +0200

    .................

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import { useQuery } from "@tanstack/react-query";
 
 function Demo() {
+  const { data: todos, isLoading } = useQuery({
+    queryFn: () => fetchTodos(),
+    queryKey: ["todos"],
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div>
-      <h1>
+      {/* <h1>
         <b>React Query Tutorial</b>
-      </h1>
+      </h1> */}
+      {todos?.map((todo, index) => (
+        <TodoCard key={index} tod0={todo} />
+      ))}
     </div>
   );
 }
