be554fc0584c2881a6cb6ea27b4b98225304bd8b
Sara <sarailic431@gmail.com>
2025-07-15 17:34:51 +0200

    ......

--- src/Demo.jsx
+++ src/Demo.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useQuery } from "@tanstack/react-query";
+import { fetchTodos } from "./api";
 
 function Demo() {
   const { data: todos, isLoading } = useQuery({
---
+++ src/Todo.jsx
@@ -0,0 +1,4 @@
+import React from "react";
+
+function Todo() {}
+export default Todo;
